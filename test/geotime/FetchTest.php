abfb236e49b330c2d5ef3753da682e263121e8c0
Bruno Pérel <brunoperel@gmail.com>
2014-03-10 13:23:35 +0100

    Add test : fetching info for a non-existent image

--- test/geotime/FetchTest.php
+++ test/geotime/FetchTest.php
@@ -10,4 +10,13 @@ class FetchTest extends \PHPUnit_Framework_TestCase {
         $imageFile = Fetch::getCommonsImageURL('Wiki-commons', '.png');
         $this->assertNotNull($imageFile);
     }
+    public function testGetCommonsInexistantImageURL()
+    {
+        ob_start();
+        $output = Fetch::getCommonsImageURL('inexistant', '.png');
+        $echoOutput = ob_get_clean();
+
+        $this->assertStringStartsWith('<b>Error', $echoOutput);
+        $this->assertNull($output);
+    }
 } 
