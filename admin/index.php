cbc650c00bfb750b91eecbc06b975293491715ba
Bruno Pérel <brunoperel@gmail.com>
2014-03-26 14:59:22 +0100

    Fix unused JS file reference

--- admin/index.php
+++ admin/index.php
@@ -43,6 +43,5 @@ Geotime::showStatus();
 <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
 <script src="../js/plugins.js"></script>
-<script src="../js/map.js"></script>
 </body>
 </html>
