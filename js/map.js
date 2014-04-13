0763fb226b30c387fdbed7f22d5e64a7fc60c04f
Bruno Perel <brunoperel@gmail.com>
2014-04-13 23:41:40 +0200

    Add zoom/pan on background map

--- js/map.js
+++ js/map.js
@@ -1,15 +1,33 @@
 var width = 960;
+var mapHeight= 480;
 
 var projection = d3.geo.equirectangular()
 	.scale((width + 1) / 2 / Math.PI)
 	.precision(.01);
 
+var path = d3.geo.path()
+	.projection(projection);
+
+var zoom = d3.behavior.zoom()
+	.translate(projection.translate())
+	.scale(projection.scale())
+	.on("zoom", function() {
+		projection.translate(d3.event.translate).scale(d3.event.scale);
+		svg.selectAll("path").attr("d", path);
+	});
+
 var svg = d3.select("#mapArea").append("svg")
 	.attr("width", width)
 	.attr("height", mapHeight)
-	.attr("id", "map");
+	.attr("id", "map")
+	.call(zoom);
 
-showMap("background", "data/external/ne_110m_coastline.json");
+svg.append("rect")
+	.attr("id", "bg")
+	.attr("width", width)
+	.attr("height", mapHeight);
+
+showBgMap("backgroundMap", "data/external/ne_110m_coastline.json");
 
 var svgmap_drag = d3.behavior.drag()
 	.origin(function(d) { return d; })
@@ -32,20 +50,19 @@ function dragmove(d) {
 		.style("margin-top",+ d.y+"px");
 }
 
-function showMap(id, filePath) {
-	d3.json(filePath, function(error, world) {
-		var path = d3.geo.path()
-			.projection(projection);
+function showBgMap(id, filePath) {
+		d3.json(filePath, function(error, world) {
 
 		svg.append("g")
 			.attr("id", id)
-			.selectAll(".subunit")
+				.selectAll(".subunit")
 			.data(world.features)
-			.enter().append("path")
-			.attr("class", function(d) {
-				return "subunit-boundary subunit " + d.properties.adm0_a3;
-			})
-			.attr("d", path);
+			.enter()
+				.append("path")
+				.attr("class", function(d) {
+					return "subunit-boundary subunit " + d.properties.adm0_a3;
+				})
+				.attr("d", path);
 	});
 }
 
@@ -62,7 +79,6 @@ function dragresizestarted() {
 }
 
 function dragresize(){
-	var foo = new Date();
 	svgMap.datum().width += d3.event.dx;
 	svgMap.datum().height += d3.event.dy;
 
