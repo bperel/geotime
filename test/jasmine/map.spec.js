544fb591d2f6f28b690b4df1f18d910f09f5f6a4
Bruno Pérel <brunoperel@gmail.com>
2014-12-23 16:40:36 +0100

    Init external map tests

--- test/jasmine/map.spec.js
+++ test/jasmine/map.spec.js
@@ -12,6 +12,10 @@ describe('Map tests', function() {
         jasmine.Ajax.install();
     });
 
+    afterEach(function() {
+        jasmine.Ajax.uninstall();
+    });
+
     describe('Map placeholders', function() {
         it('should be loaded', function() {
             expect(d3.select("#mapHelper").empty()).toBeFalsy();
@@ -31,5 +35,59 @@ describe('Map tests', function() {
             showBgMap("backgroundMap", getJSONFixture("test/geotime/_data/countries.json"));
             expect(d3.select("#backgroundMap").empty()).toBeFalsy();
         })
-    })
+    });
+
+    describe('External map', function() {
+        var territoryName = 'simple';
+        var territoryFilename = 'simple.svg';
+
+        beforeEach(function(){
+
+            jasmine.getFixtures().fixturesPath = 'base';
+            helperStepsData = [{
+                step: 1, content: ['Step 1 description'],
+                dataUpdate: function() {},
+                buttons: []
+            }];
+
+            initMapArea();
+            showBgMap("backgroundMap", getJSONFixture("test/geotime/_data/countries.json"));
+
+            spyOn(window, "ajaxPost").and.callFake(function(options, callback) {
+                var response;
+                var requestType = Object.keys(options)[0];
+                switch(requestType) {
+                    case 'getSvg':
+                        response = {
+                            id: territoryName,
+                            fileName: territoryFilename
+                        };
+                    break;
+                }
+                callback('', response);
+            });
+
+            spyOn(d3, 'xml').and.callFake(function(path, mime, callback) {
+                callback(( new window.DOMParser() ).parseFromString(
+                    '<svg width="110" height="60" id="simple" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+                        +'<rect x="10" y="10" height="50" width="100" style="stroke:#ff0000; fill: #0000ff"/>'
+                    +'</svg>',
+                    mime)
+                );
+            });
+        });
+
+        it('should load', function() {
+            loadTerritoryMap();
+
+            expect(svgMap).toBeDefined();
+            expect(svgMap.attr('name')).toEqual(territoryFilename);
+            expect(svgMap.datum().x).toEqual(0);
+            expect(svgMap.datum().y).toEqual(0);
+            expect(svgMap.datum().width).toEqual(110);
+            expect(svgMap.datum().height).toEqual(60);
+            expect(svgMap.style("margin-left")).toEqual('0px');
+            expect(svgMap.style("margin-top")).toEqual('0px');
+        });
+    });
 });
