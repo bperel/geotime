f6307f5db2e511b26641117868de5c102e2e4b69
Bruno Perel <brunoperel@gmail.com>
2015-06-25 23:14:01 +0200

    Fix JS dependencies

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 requirejs.config({
+	urlArgs: "v=" + (new Date()).getTime(),
 	"paths": {
 		"js": "js"
 	},
 	"shim": {
-		"d3.xpath": ["vendor/d3/d3.min"]
+		"d3.xpath": ["vendor/d3/d3.min"],
+		"helper": ["vendor/d3/d3.min"],
+		"helper.config.calibration": ["helper"],
+		"util": ["vendor/d3/d3.min"],
+		"map": ["vendor/d3/d3.min"]
 	}
 });
 
