b79644b4d2047085062daf3de12ea6b1903bd069
Bruno Perel <brunoperel@gmail.com>
2015-06-30 21:35:13 +0200

    Add tests for buildAndSaveFromObject

--- test/phpunit/new_models/ReferencedTerritoryTest.php
+++ test/phpunit/new_models/ReferencedTerritoryTest.php
@@ -54,11 +54,45 @@ class ReferencedTerritoryTest extends MariaDbTestHelper {
         $this->assertEquals(count($newReferencedTerritory->getPrevious()), 1);
         $this->assertEquals(count($newReferencedTerritory->getNext()), 1);
 
+        /** @var ReferencedTerritory $previousReferencedTerritory */
         $previousReferencedTerritory = $this->getRepository()->findOneBy(array('name' => $previousReferencedTerritoryName));
         $this->assertNotNull($previousReferencedTerritory);
         $this->assertEquals(count($previousReferencedTerritory->getPrevious()), 0);
         $this->assertEquals(count($previousReferencedTerritory->getNext()), 0);
+    }
 
+    public function testBuildAndSaveFromObject() {
+        $object = json_decode(json_encode(array(
+            'name' => array('value' => 'New territory'),
+            'previous' => array('value' => 'The previous territory'),
+            'next' => array('value' => 'The next territory|Another next territory')
+        )));
+        ReferencedTerritoryHelper::buildAndSaveFromObject($object);
+
+        /** @var ReferencedTerritory $newReferencedTerritory */
+        $newReferencedTerritory = $this->getRepository()->findOneBy(array('name' => $object->name->value));
+        $this->assertNotNull($newReferencedTerritory);
+        $this->assertEquals(count($newReferencedTerritory->getPrevious()), 1);
+        $this->assertEquals(count($newReferencedTerritory->getNext()), 2);
+
+        /** @var ReferencedTerritory $previousReferencedTerritory */
+        $previousReferencedTerritory = $this->getRepository()->findOneBy(array('name' => $object->previous->value));
+        $this->assertNotNull($previousReferencedTerritory);
+        $this->assertEquals(count($previousReferencedTerritory->getPrevious()), 0);
+        $this->assertEquals(count($previousReferencedTerritory->getNext()), 0);
+    }
+
+    public function testBuildAndSaveFromIncompleteObject() {
+        $object = json_decode(json_encode(array(
+            'name' => array('value' => 'Another new territory')
+        )));
+        ReferencedTerritoryHelper::buildAndSaveFromObject($object);
+
+        /** @var ReferencedTerritory $newReferencedTerritory */
+        $newReferencedTerritory = $this->getRepository()->findOneBy(array('name' => $object->name->value));
+        $this->assertNotNull($newReferencedTerritory);
+        $this->assertEquals(count($newReferencedTerritory->getPrevious()), 1);
+        $this->assertEquals(count($newReferencedTerritory->getNext()), 2);
     }
 }
  
