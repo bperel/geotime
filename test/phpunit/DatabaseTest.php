3bb120285f9a866ce7f40790102aa8e27f2e2f22
Bruno Perel <brunope@qliro.local>
2015-07-10 09:15:01 +0200

    Update Database test

--- test/phpunit/DatabaseTest.php
+++ test/phpunit/DatabaseTest.php
@@ -2,48 +2,19 @@
 
 namespace geotime\Test;
 
-use geotime\Database;
-use geotime\models\CriteriaGroup;
-use PHPUnit_Framework_TestCase;
+use geotime\helpers\ModelHelper;
+use geotime\Test\Helper\MariaDbTestHelper;
 
 
-class DatabaseTest extends \PHPUnit_Framework_TestCase {
-
-    static $jsonSourceDir = 'test/phpunit/_data';
-
-    public function testConnect() {
-        Database::connect(Database::$testDbName);
-        $this->assertTrue(Database::$connected);
-    }
+class DatabaseTest extends MariaDbTestHelper {
 
     /* Tests */
 
-    public function testImportFromJson() {
-        CriteriaGroup::drop();
-
-        $this->assertEquals(0, CriteriaGroup::count());
-        $nbImportedObjects = CriteriaGroup::importFromJson(self::$jsonSourceDir.'/criteriaGroups.json');
-        $this->assertEquals(2, CriteriaGroup::count());
-        $this->assertEquals(2, $nbImportedObjects);
-    }
-
-    public function testImportFromJsonInvalidFileName() {
+    public function testConnect() {
         try {
-            CriteriaGroup::importFromJson(self::$jsonSourceDir.'/criteriaGroups-1-.json');
+            ModelHelper::getEm()->getConnection()->connect();
+        } catch (\Exception $e) {
             $this->fail();
-        }
-        catch (\InvalidArgumentException $e) {
-            $this->assertStringStartsWith('Invalid file name for JSON import', $e->getMessage());
-        }
-    }
-
-    public function testImportFromJsonInexistentFile() {
-        try {
-            CriteriaGroup::importFromJson(self::$jsonSourceDir . '/criteriaGroups2.json');
-            $this->fail();
-        }
-        catch (\InvalidArgumentException $e) {
-            $this->assertStringStartsWith('Error on JSON import', $e->getMessage());
         }
     }
 }
