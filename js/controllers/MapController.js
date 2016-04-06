c646eb17bfd10368c46c70a2928ff18eb7c331dd
Bruno Perel <brunoperel@gmail.com>
2016-04-06 23:31:59 +0200

    Remove the step concept altogether

--- js/controllers/MapController.js
+++ js/controllers/MapController.js
@@ -5,50 +5,14 @@ geotimeControllers.controller('MapController', ['$scope', '$state',
 		$scope.mapInfo = null;
 
 		$scope.processes = [];
-		$scope.steps = helperStepsData;
-
-		$scope.activeProcess = null;
-		$scope.activeStep = null;
-
+		
 		$scope.resizeBackgroundMap = function(width, height) {
 			$scope.bgMapWidth = width;
 			$scope.bgMapHeight = height;
 		};
 
-		$scope.isActiveProcess = function(processName) {
-			return processName === $scope.activeProcess;
-		};
-
-		$scope.isActiveStep = function (step) {
-			return $scope.activeStep 	=== step.order
-				&& $scope.activeProcess === step.process;
-		};
-
-		$scope.initSteps = function() {
-			$scope.steps = [];
-			// angular.forEach(helperStepsData, function(step) {
-			// 	if ($scope.isActiveProcess(step.process)) {
-			// 		$scope.steps.push(step);
-			// 	}
-			// });
-		};
-
-		$scope.unloadCurrentStep = function() {
-			((helperStepsData.filter($scope.isActiveStep)[0] || {})
-				.onUnload || []
-			)
-			.forEach(function (onUnloadAction) {
-				onUnloadAction();
-			});
-		};
-
 		$scope.loadProcess = function(processName) {
-			$scope.unloadCurrentStep();
-			$scope.activeProcess = processName;
-			$scope.activeStep = 1;
 			$state.go('app.map-placeholders.'+processName);
-
-			$scope.initSteps();
 		};
 
 		$scope.loadTerritoryMap = function(noUi) {
